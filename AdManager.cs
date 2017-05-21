21cdb9beb12cc0437a6af1d1186b576fedc2c7c6
alexstat123 <alexander.statsenko198@gmail.com>
2017-05-21 09:49:06 +0100

    Add files via upload

---
+++ AdManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+//using admob;
+
+public class AdManager : MonoBehaviour {
+
+	public static AdManager Instance { set; get;}
+
+	public string bannerIs;
+	public string VideoId;
+	// Use this for initialization
+	void Start () {
+
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+		//Admob.Instance ().initAdmob (bannerIs,VideoId);
+		//Admob.Instance().setTesting(true);
+		//Admob.Instance ().loadInterstitial ();
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showBanner()
+	{
+		#if UNITY_EDITOR
+		#elif UNITY_ANDROID
+		//Admob.Instance ().showBannerRelative (AdSize.Banner,AdPosition.BOTTOM_CENTER,5);
+		#endif
+	}
+
+	public void showVideo()
+	{
+		#if UNITY_EDITOR
+		#elif UNITY_ANDROID
+		//if(Admob.Instance().isInterstitialReady()){
+
+			//Admob.Instance().showInterstitial();
+
+		//}
+
+		#endif
+
+	}
+
+
+}
