486491031235fb8845d023d10db23b14084e3650
alexstat123 <alexander.statsenko198@gmail.com>
2017-05-21 09:54:11 +0100

    Add files via upload

---
+++ FauxGravityAttractor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+	//public float gravity = 10;
+
+	//public void Attract(Transform body){
+
+		//Vector3 gravityUp = (body.position - transform.position).normalized;
+		//Vector3 localUp = body.up;
+
+		//body.GetComponent<Rigidbody> ().AddForce (gravityUp*gravity);
+
+		//Quaternion targetRotation = Quaternion.FromToRotation (localUp, gravity) * body.rotation;
+		//body.rotation = Quaternion.Slerp (body.rotation, targetRotation, 50f * Time.deltaTime);
+	//}
+
+
+}
---
+++ FauxGravityBody.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody> ().useGravity = false;
+		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeRotation;
+
+		myTransform = transform;
+	}
+	
+
+	void FixedUpdate () {
+
+		//if (attractor){
+			//attractor.Attract (myTransform);
+		//}
+		
+	}
+}
